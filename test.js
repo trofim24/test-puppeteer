82b31c93c8fae249ab45380fa612bc61a250e6a3
Alexander Trofimov <Alexander.Trofimov@onlyoffice.com>
2025-05-04 17:34:37 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,32 +1,26 @@
-import puppeteer from 'puppeteer';
+const puppeteer = require('puppeteer');
 
 (async () => {
-  // Launch the browser and open a new blank page
-  const browser = await puppeteer.launch();
+  // Launch the browser
+  const browser = await puppeteer.launch({
+    headless: "new", // Use the new headless mode
+    // If you want to see the browser UI, set to false
+    // headless: false,
+  });
+  
+  // Create a new page
   const page = await browser.newPage();
-
-  // Navigate the page to a URL
-  await page.goto('https://developer.chrome.com/');
-
-  // Set screen size
-  await page.setViewport({width: 1080, height: 1024});
-
-  // Type into search box
-  await page.type('.devsite-search-field', 'automate beyond recorder');
-
-  // Wait and click on first result
-  const searchResultSelector = '.devsite-result-item-link';
-  await page.waitForSelector(searchResultSelector);
-  await page.click(searchResultSelector);
-
-  // Locate the full title with a unique string
-  const textSelector = await page.waitForSelector(
-    'text/Customize and automate',
-  );
-  const fullTitle = await textSelector?.evaluate(el => el.textContent);
-
-  // Print the full title
-  console.log('The title of this blog post is "%s".', fullTitle);
-
+  
+  // Navigate to a URL
+  await page.goto('https://example.com');
+  
+  // Take a screenshot
+  await page.screenshot({ path: 'example.png' });
+  
+  // Get page title
+  const title = await page.title();
+  console.log('Page title:', title);
+  
+  // Close browser
   await browser.close();
 })();
